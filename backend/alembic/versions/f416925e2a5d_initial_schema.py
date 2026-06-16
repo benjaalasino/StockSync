8a71ce688df246aa1a70f5ac027f8223d168966f
joacoContreras <cjoaquin835@gmail.com>
2026-06-16 19:06:37 +0000

    fix lint

--- backend/alembic/versions/f416925e2a5d_initial_schema.py
+++ backend/alembic/versions/f416925e2a5d_initial_schema.py
@@ -1,21 +1,21 @@
 """initial_schema
 
 Revision ID: f416925e2a5d
-Revises: 
+Revises:
 Create Date: 2026-06-16 18:22:04.355770
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'f416925e2a5d'
-down_revision: Union[str, None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
